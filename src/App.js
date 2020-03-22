7544ae5149eaa22294c595051f9848cb24cfa852
ivanvlizarazo <ivanlizarazo1305@gmail.com>
2020-03-21 22:50:16 -0500

    se hace la instalación de todo lo relacionado con la conección a la base de datos y se traen los datos para mostrarse en consola

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from "react";
+import { useSubscription } from "@apollo/react-hooks";
+import sql from "graphql-tag";
 import logo from "./logo.svg";
-import "./App.css";
+
+import "./App.scss";
+
+const GET_POST = sql`
+subscription GetStop {
+  stop(where: {game_id: {_eq: 1}, player_id: {_neq: 2}}) {
+    animal
+    apellido
+    ciudad
+    color
+    cosa
+    fruta
+    nombre
+    pais
+  }
+}
+
+`;
 
 function App() {
+  const { loading, error, data } = useSubscription(GET_POST);
+
+  if (loading) {
+    console.log("cargando");
+  } else {
+    console.log("data: ", data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+
+import { ApolloProvider } from "@apollo/react-hooks";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import connection from "./connection";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={connection}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
