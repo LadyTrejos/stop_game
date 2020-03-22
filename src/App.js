4a8353fdebaecb08b41834ba4b01e8e0132119d2
ivanvlizarazo <ivanlizarazo1305@gmail.com>
2020-03-22 01:42:58 -0500

    se trae el stop de un jugador en específico haciendo la consulta con variables y se insertan nuevas partidas a la base de datos

--- src/App.js
+++ src/App.js
@@ -1,38 +1,75 @@
-import React from "react";
-import { useSubscription } from "@apollo/react-hooks";
-import sql from "graphql-tag";
+import React, { useState } from "react";
+import { useSubscription, useMutation, useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 import logo from "./logo.svg";
 
 import "./App.scss";
 
-
-const GET_POST = sql`
-subscription GetStop {
-  stop(where: {game_id: {_eq: 1}, player_id: {_neq: 2}}) {
-    animal
-    apellido
-    ciudad
-    color
-    cosa
-    fruta
-    nombre
-    pais
+const GET_POST = gql`
+  subscription GetStop($game_id: Int!, $player_id: Int!) {
+    stop(
+      where: { game_id: { _eq: $game_id }, player_id: { _neq: $player_id } }
+    ) {
+      animal
+      apellido
+      ciudad
+      color
+      cosa
+      fruta
+      nombre
+      pais
+    }
   }
-}
+`;
+
+const GET_POST2 = gql`
+  subscription GetStop($game_id: Int!, $player_id: Int!) {
+    stop(
+      where: { game_id: { _eq: $game_id }, player_id: { _neq: $player_id } }
+    ) {
+      animal
+      apellido
+      ciudad
+      color
+      cosa
+      fruta
+      nombre
+      pais
+    }
+  }
+`;
+
+const INSERT_GAME = gql`
+  mutation InsertGame {
+    insert_games(objects: {}) {
+      returning {
+        id
+      }
+    }
+  }
 `;
 
 function App() {
-  const { loading, error, data } = useSubscription(GET_POST);
+  const [InsertGame] = useMutation(INSERT_GAME);
+  const [gameID, setGameID] = useState(null);
+  const [playerID, setPlayerID] = useState(null);
+  const { loading, error, data } = useSubscription(GET_POST, {
+    variables: { game_id: gameID, player_id: playerID }
+  });
 
   if (loading) {
     console.log("cargando");
   } else {
-    console.log("data: ", data)}
-
-  function newGame() {
-    console.log("nueva partida");
+    console.log("data: ", data);
   }
 
+  function newGame() {
+    // InsertGame(); //descomentar
+    console.log("nueva partida");
+    setGameID(1);
+    setPlayerID(2);
+  }
+  let stops = null;
   return (
     <div className="App">
       <div className="stop-title">Stop!</div>
