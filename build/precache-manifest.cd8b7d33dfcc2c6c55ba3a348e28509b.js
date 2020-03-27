f6d9bec18870aa0fe744638f9897e565a40cf7a2
Lady Trejos <ladytrejos@utp.edu.co>
2020-03-27 02:02:21 -0500

    add build and run dev script

---
+++ build/precache-manifest.cd8b7d33dfcc2c6c55ba3a348e28509b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3b67cd1978e7f5696cb1b3058f13fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "148ec07c119490b79096",
+    "url": "/static/css/main.d314cea7.chunk.css"
+  },
+  {
+    "revision": "5408fe1f0a86ffb50966",
+    "url": "/static/js/2.a892ff6a.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.a892ff6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148ec07c119490b79096",
+    "url": "/static/js/main.e5ad6d8e.chunk.js"
+  },
+  {
+    "revision": "648cef59bdce564feb54",
+    "url": "/static/js/runtime-main.5734360f.js"
+  }
+]);
